d04ce9674bda2b11c4e0b2a120670d148943d7d4
Thomas Petersohn <26284950+tpetersohn@users.noreply.github.com>
2024-06-03 18:15:20 +0200

    modified:   transducer.js

--- transducer.js
+++ transducer.js
@@ -1,7 +1,7 @@
 import crypto from "node:crypto"
 
-const shuffleArray = (arr) => {
-
+const shuffleArray = (ar) => {
+  let arr = ar.slice(0)
   for (let i = arr.length - 1; i > 0; i--) {
       const j = crypto.randomInt(0,i)
       {[arr[i], arr[j]] = [arr[j], arr[i]]}
@@ -16,14 +16,14 @@ const concat      = (xs, x)     => xs.concat(x)
 
     , isOdd       = (x)         => x % 2 !== 0
     , isEven      = (x)         => x % 2 == 0
-    , UpLoEven    = (x)         => isEven(x.charCodeAt(0)) ? x.toLowerCase(x) : x.toUpperCase(x)
-    , UpLoOdd     = (x)         => isOdd(x.charCodeAt(0)) ? x.toLowerCase(x) : x.toUpperCase(x)
-    , isString    = (x)         => x.constructor == String
-    , isNumber    = (x)         => x.constructor == Number
+    , upperEven   = (x)        => isEven(x.charCodeAt(0)) ? x.toUpperCase(x) : x
+    , upperOdd    = (x)        => isOdd(x.charCodeAt(0)) ? x.toUpperCase(x) : x
+    , isString    = (x)         => typeof x === 'string' || x instanceof String
+    , isNumber    = (x)         => typeof x === 'number' || x instanceof Number
     , removeLower = (x)         => /^[A-Z]$/.test(x)
     , removeUpper = (x)         => /^[a-z]$/.test(x)
-    , fillEmpty   = (x)         => x === " " ? "ø" : x
-    , toString    = (x)         => isNumber(x) ? x = String(x) : x
+    , fillEmpty   = (x)         => /^\s*$/.test(x) ? "ø" : x
+    , toString    = (x)         => isNumber(x) ? String(x) : x
 
 
     , tap         = (fn, msg)   => (rf) => (acc, x)   => {if(acc.length>=26) {fn([...acc,x], msg)}; return rf(acc,x)}
@@ -35,14 +35,17 @@ const concat      = (xs, x)     => xs.concat(x)
 
                     (
 
+                      
                       tap(console.log, "before toString"),
                       mapTf(toString),
-                      filterTf(isOdd),
-                      tap(console.log, "before uploOdd"),
-                      mapTf(UpLoOdd), 
+                      tap(console.log, "before upperOdd"),
+                      mapTf(upperOdd),
+                      tap(console.log, "before removeUpper"),
+                      filterTf(removeUpper),
+                      tap(console.log, "before fillEmpty"),
+                      mapTf(fillEmpty),
                       tap(console.log, "before shuffle"),
                       applyTf(shuffleArray),
-                      mapTf(fillEmpty),
          
                     )
 
